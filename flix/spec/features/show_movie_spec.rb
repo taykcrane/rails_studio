ee338bee1c06089648ba6610bb4e2b235b691c68
Taylor Crane <taylorkcrane@gmail.com>
2015-11-14 23:15:02 -0500

    stuff

--- flix/spec/features/show_movie_spec.rb
+++ flix/spec/features/show_movie_spec.rb
@@ -2,18 +2,29 @@ require "spec_helper"
 
 describe "Viewing an individual movie" do
 	it "shows the movie's details" do
-		movie = Movie.create(title: "Iron Man",
-                       rating: "PG-13",
-                       total_gross: 318412101.00,
-                       description: "Tony Stark builds an armored suit to fight the throes of evil",
-                       released_on: "2008-05-02")
+		movie = Movie.create(movie_attributes)
 
 		visit movie_url(movie.id)
 
 		expect(page).to have_text(movie.title)
   		expect(page).to have_text(movie.rating)
-  		expect(page).to have_text("$318,412,101.00")
   		expect(page).to have_text(movie.description)
   		expect(page).to have_text(movie.released_on)
 	end
+
+  it "it shows the total_gross if total_gross exceeds $50M" do
+    movie = Movie.create(movie_attributes(total_gross: 60000000))
+
+    visit movie_url(movie.id)
+
+    expect(page).to have_text("$60,000,000.00")
+  end
+
+  it "shows 'Flop!' if total_gross is less than $50M" do
+    movie = Movie.create(movie_attributes(total_gross: 1000))
+
+    visit movie_url(movie.id)
+
+    expect(page).to have_text("Flop!")
+  end
 end
