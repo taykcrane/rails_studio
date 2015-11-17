624c5220ddcced13d454136b90468816f30a75ec
Taylor Crane <taylorkcrane@gmail.com>
2015-11-17 18:27:58 -0500

    learning new and create, and partials

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -13,9 +13,23 @@ class MoviesController < ApplicationController
 
 	def update
 		@movie = Movie.find(params[:id])
-		movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
 		@movie.update(movie_params)
 
 		redirect_to movie_path(@movie.id)
+	end
+
+	def new
+		@movie = Movie.new()
+	end
+
+	def create
+		@movie = Movie.new(movie_params)
+		@movie.save
+		redirect_to movie_path(@movie.id)
+	end
+
+	private
+	def movie_params
+		params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
 	end
 end
