24d5b074c16275cb0aa6c350f1273531bc254cb8
Taylor Crane <taylorkcrane@gmail.com>
2015-11-20 17:18:24 -0500

    beginning to add many models

--- flix/app/models/movie.rb
+++ flix/app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
-  
+  has_many :reviews
+
+
   RATINGS = ["G", "PG", "PG-13", "R", "NC-17"]
 
   validates :title, :released_on, :duration, presence: true
--- flix/config/routes.rb
+++ flix/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :reviews
+
   root "movies#index"
   resources :movies
 end
