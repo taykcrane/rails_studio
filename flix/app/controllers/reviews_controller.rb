937b06e0377aad1f15a1c9406fa83a038860b1a8
Taylor Crane <taylorkcrane@gmail.com>
2015-11-25 22:50:00 -0500

    deleting and editing reviews

--- flix/app/controllers/reviews_controller.rb
+++ flix/app/controllers/reviews_controller.rb
@@ -19,6 +19,25 @@ class ReviewsController < ApplicationController
 		end
 	end
 
+	def edit
+		@review = @movie.reviews.find(params[:id])
+	end
+
+	def update
+		@review = @movie.reviews.find(params[:id])
+		if @review.update(review_params)
+			redirect_to movie_reviews_path(@movie), notice: "Review successfully updated!"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@review = @movie.reviews.find(params[:id])
+		@review.destroy
+		redirect_to movie_path(@movie), alert: "Movie successfully deleted!"
+	end
+
 
 
 	private
